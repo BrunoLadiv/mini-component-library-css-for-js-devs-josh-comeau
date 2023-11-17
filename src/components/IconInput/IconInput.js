7a622c315c0e3227e272552e874058be7201310c
Bruno Vidal <brunokasprik@gmail.com>
2023-11-16 21:23:53 -0300

    iconinput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -11,9 +11,39 @@ const IconInput = ({
   icon,
   width = 250,
   size,
-  placeholder,
+  ...rest
 }) => {
-  return 'TODO';
+  return (
+    <Wrapper>
+      <IconWrapper>
+        <Icon id={icon}/>
+      </IconWrapper>
+      
+      <StyleInput {...rest} type="text" />
+      <VisuallyHidden>{label}</VisuallyHidden>
+    </Wrapper>
+  );
 };
+
+const IconWrapper = styled.div`
+  position: absolute;
+`
+const Wrapper = styled.label`
+  position: relative;
+
+`
+const StyleInput = styled.input`
+  height: 1.6rem;
+  border: none;
+  border-bottom: 1px solid black;
+  padding-left: 29px;
+  color: ${COLORS.gray700};
+  font-weight: 700;
+  outline-offset: 3px;
+  &::placeholder{
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+  `
 
 export default IconInput;
